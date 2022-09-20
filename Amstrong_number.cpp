795161125bd3c293b2814ebaa848ab762171422a
LeDinhNguyen <ledinhnguyen00@gmail.com>
2022-09-20 18:15:18 +0700

    Find Amstrong number

--- Amstrong_number.cpp
+++ Amstrong_number.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 
+void Amstrongs_number(int num)
+{
+	int n = num;
+	int sum = 0;
+	int a;
+	
+	while (n > 0) {
+		a = n % 10;
+		sum += (a*a*a);
+
+		n /= 10;
+	}
+	if (num != sum) cout << "No amstrongs";
+	else cout << "amstrongs";
+}
+
 int main()
 {
+	int number;
+	cout << "Nhap so nguyen n: ";
+	cin >> number;
+
+	Amstrongs_number(number);
+	
 	return 0;
 }
